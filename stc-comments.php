3394576a7a484c5756d5cf03f086c476fe96bb85
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-13 17:25:07 +0000

    Found a profile image URL that still works on Twitter
    git-svn-id: https://plugins.svn.wordpress.org/simple-twitter-connect/trunk@726327 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stc-comments.php
+++ stc-comments.php
@@ -169,7 +169,7 @@ function stc_comm_get_display() {
 	$tw = stc_get_credentials();
 	if ($tw) {
 		echo "<span id='tw-user'>".
-			 "<!--<img src='http://api.twitter.com/1/users/profile_image/".$tw->screen_name."?size=bigger' width='96' height='96' />-->".
+			 "<img src='http://twitter.com/api/users/profile_image/".$tw->screen_name."?size=bigger' width='96' height='96' />".
 			 "<span id='tw-msg'><strong>Hi ".$tw->name."!</strong><br />You are connected with your Twitter account. ".
 			 "<a href='?stc-logout=1'>Logout</a>".
 			 "</span></span>";
@@ -260,7 +260,7 @@ function comment_user_details_end() { echo '</div>'; }
 
 
 // generate avatar code for Twitter user comments
-//add_filter('get_avatar','stc_comm_avatar', 10, 5);
+add_filter('get_avatar','stc_comm_avatar', 10, 5);
 function stc_comm_avatar($avatar, $id_or_email, $size = '96', $default = '', $alt = false) {
 	// check to be sure this is for a comment
 	if ( !is_object($id_or_email) || !isset($id_or_email->comment_ID) || $id_or_email->user_id) 
@@ -270,7 +270,7 @@ function stc_comm_avatar($avatar, $id_or_email, $size = '96', $default = '', $al
 	$twuid = get_comment_meta($id_or_email->comment_ID, 'twuid', true);
 	if ($twuid) {
 		// return the avatar code
-		$avatar = "<img class='avatar avatar-{$size} twitter-avatar' src='http://api.twitter.com/1/users/profile_image/{$twuid}?size=bigger' width='{$size}' height='{$size}' />";
+		$avatar = "<img class='avatar avatar-{$size} twitter-avatar' src='http://twitter.com/api/users/profile_image/{$twuid}?size=bigger' width='{$size}' height='{$size}' />";
 	}
 		
 	return $avatar;
